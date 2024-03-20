095f6cc7ea44d6ad3440c4e140ac83a0f64cf008
cuboalcub <fvera8192@gmail.com>
2024-03-20 15:36:19 -0600

    add login and home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LandingPageComponent]
+    imports: [RouterOutlet, LandingPageComponent, RouterLink, CommonModule]
 })
 export class AppComponent {
   title = 'miselania';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent }
+ ];
