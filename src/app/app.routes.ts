06d0137767e234b53c9bc17a299a2f29285747c5
cuboalcub <fvera8192@gmail.com>
2024-03-22 13:47:24 -0600

    update login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
+import { ForgotPaswordComponent } from './forgot-pasword/forgot-pasword.component';
+import { NewUserComponent } from './new-user/new-user.component';
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'home', component: HomeComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'new-user', component: NewUserComponent },
+    { path: 'forgot-password', component: ForgotPaswordComponent }
  ];
---
+++ src/app/forgot-pasword/forgot-pasword.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-pasword',
+  standalone: true,
+  imports: [],
+  templateUrl: './forgot-pasword.component.html',
+  styleUrl: './forgot-pasword.component.css'
+})
+export class ForgotPaswordComponent {
+
+}
