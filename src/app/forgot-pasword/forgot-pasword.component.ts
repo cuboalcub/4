c1afdeacf04c24259fa62aa7c91eb8058a124d43
cuboalcub <fvera8192@gmail.com>
2024-03-30 10:12:04 -0600

    add all

--- src/app/forgot-pasword/forgot-pasword.component.ts
+++ src/app/forgot-pasword/forgot-pasword.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet,RouterLinkActive } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-forgot-pasword',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './forgot-pasword.component.html',
   styleUrl: './forgot-pasword.component.css'
 })
--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet,RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-new-user',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './new-user.component.html',
   styleUrl: './new-user.component.css'
 })
